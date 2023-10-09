5fa2c91d66dc92ac908c4439720baceee1e6a709
PSewmuthu <apasindusewmuthu@gmail.com>
2023-10-09 19:15:59 +0530

    Defined Category model

--- FoodOrder.Models/Category.cs
+++ FoodOrder.Models/Category.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FoodOrder.Models
 {
-    internal class Category
+    public class Category
     {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
     }
 }
