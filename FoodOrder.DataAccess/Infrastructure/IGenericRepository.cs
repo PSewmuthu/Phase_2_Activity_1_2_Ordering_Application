b759745b84de76e1fcf9faa3abf638f468cb9168
PSewmuthu <apasindusewmuthu@gmail.com>
2023-10-09 19:32:53 +0530

    Created IGenericRepository.cs file in Infrastructure in DataAccess

---
+++ FoodOrder.DataAccess/Infrastructure/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrder.DataAccess.Infrastructure
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? predicate=null,string? includeproperties=null);
+        T GetById(Expression<Func<T, bool>>? predicate = null, string? includeproperties = null);
+        void Insert(T entity);
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entities);
+    }
+}
