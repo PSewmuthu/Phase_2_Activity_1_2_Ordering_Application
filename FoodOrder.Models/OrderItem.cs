94a186224bc0723e7f9040d2d60b0dba4d8f289b
PSewmuthu <apasindusewmuthu@gmail.com>
2023-10-10 00:52:24 +0530

    Created OrderItem.cs file in Models

---
+++ FoodOrder.Models/OrderItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrder.Models
+{
+    public class OrderItem
+    {
+        public OrderItem(int itemId, decimal unitPrice, int quantity, decimal total)
+        {
+            ItemId = itemId;
+            UnitPrice = unitPrice;
+            Quantity = quantity;
+            Total = total;
+        }public int Id { get; set; }
+        public int ItemId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal Total { get; set; }
+        public string OrderId { get; set; }
+        public virtual Order Order { get; set; }
+    }
+}
