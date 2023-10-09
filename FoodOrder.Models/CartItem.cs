33cc0920a8d5531342c08233a02d0f168518f0ab
PSewmuthu <apasindusewmuthu@gmail.com>
2023-10-10 00:50:53 +0530

    Created CartItem.cs file in Models

---
+++ FoodOrder.Models/CartItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FoodOrder.Models
+{
+    public class CartItem
+    {
+        public CartItem(int itemId, int quantity, decimal unitPrice)
+        {
+            ItemId = itemId;
+            UnitPrice = unitPrice;
+            Quantity = quantity;
+        }
+        public int Id { get; set; }
+        public Guid CartId { get; set; }
+        public int ItemId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        [JsonIgnore]
+        public Cart Cart { get; set; }
+    }
+}
