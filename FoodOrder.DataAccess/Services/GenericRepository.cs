c6013ff1354fb6e5544d3ab9054facbc57da4b28
PSewmuthu <apasindusewmuthu@gmail.com>
2023-10-09 22:27:59 +0530

    Created GenericRepository.cs file in Services in DataAccess

---
+++ FoodOrder.DataAccess/Services/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using FoodOrder.DataAccess.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrder.DataAccess.Services
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? predicate = null, string? includeproperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(Expression<Func<T, bool>>? predicate = null, string? includeproperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
