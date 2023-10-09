5ed9d8df132f9735365086f94869ca645306b33a
PSewmuthu <apasindusewmuthu@gmail.com>
2023-10-10 00:50:31 +0530

    Created Cart.cs file in Models

---
+++ FoodOrder.Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrder.Models
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; } = true;
+        public List<CartItem> Items { get; set; } = new List<CartItem>();
+    }
+}
