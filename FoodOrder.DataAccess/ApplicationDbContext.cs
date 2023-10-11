b7ea28f1078d968b972486ae2807f6d3d5bfba3c
PSewmuthu <apasindusewmuthu@gmail.com>
2023-10-12 02:28:31 +0530

    Updated ApplicationDbContext.cs file with models

--- FoodOrder.DataAccess/ApplicationDbContext.cs
+++ FoodOrder.DataAccess/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FoodOrder.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,11 +9,20 @@ using System.Threading.Tasks;
 
 namespace FoodOrder.DataAccess
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User,Role,int>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>optios):base()
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>optios):base(optios)
         {
-
+            
         }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemType> ItemTypes { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
     }
 }
